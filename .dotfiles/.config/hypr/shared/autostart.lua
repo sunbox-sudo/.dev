92560e9399847cccf6a691152d5791e59780a7b0
Linus Nilsson <Sunbox@sunboxx.dev>
2026-06-23 15:19:21 +0200

    uppdated autostart

--- .dotfiles/.config/hypr/shared/autostart.lua
+++ .dotfiles/.config/hypr/shared/autostart.lua
@@ -2,12 +2,13 @@
 -- Programs launched when Hyprland starts.
 -- This file is loaded last so host configs can override variables before these run.
 
-local terminal = "kitty"
+local TERMINAL= "kitty"
+local BROWSER = "zen-browser"
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd(terminal,                                     { workspace = "1 silent" })
-    hl.exec_cmd(terminal,                                     { workspace = "2 silent" })
-    hl.exec_cmd("qutebrowser",				      { workspace = "4 silent" })
+    hl.exec_cmd(TERMINAL,                                     { workspace = "1 silent" })
+    hl.exec_cmd(TERMINAL,                                     { workspace = "2 silent" })
+    hl.exec_cmd(BROWSER,				      { workspace = "4 silent" })
     hl.exec_cmd("vesktop",                                    { workspace = "5 silent" })
-    hl.exec_cmd("spotify",	                              { workspace = "7 silent" })
+    hl.exec_cmd("flatpak run com.spotify.Client",	      { workspace = "7 silent" })
 end)
